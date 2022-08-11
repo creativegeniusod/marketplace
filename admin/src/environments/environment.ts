d157cb14073f36fe016fb42695746f2804c1fcf5
devtest6785@gmail.com <devtest6785@gmail.com>
2022-08-11 11:50:44 +0530

    evv error resolved

--- admin/src/environments/environment.ts
+++ admin/src/environments/environment.ts
@@ -4,10 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://stagingwebsites.info:9000/api', // <Your API base url>
-  imageUrl: 'http://stagingwebsites.info:9000/api/media/image-resize/' // <Your API url for image resize>
-  // baseUrl: 'http://localhost:9000/api', // <Your API base url>
-  // imageUrl: 'http://localhost:9000/api/media/image-resize/', // <Your API url for image resize>
+  baseUrl: 'http://stagingwebsites.info:9000/api',
+  imageUrl: 'http://stagingwebsites.info:9000/api/media/image-resize/',
   productUrl: '', // <Your store base url>
   pluginUrl: '' //<Your API url>
 };

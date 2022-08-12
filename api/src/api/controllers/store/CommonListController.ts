b4a8bb8efdaffd2415a51a880f78221c85e1fc4a
jasvirs210@gmail.com <devtest6785@gmail.com>
2022-08-12 12:20:20 +0530

    new api commit changes

--- api/src/api/controllers/store/CommonListController.ts
+++ api/src/api/controllers/store/CommonListController.ts
@@ -72,16 +72,29 @@ export class CommonListController {
                 value: keyword,
             },
         ];
-        const WhereConditions = [
-            {
-                name: 'isActive',
-                value: 1,
-            },{
-                name: 'categoryChildId',
-                op: 'like',
-                value: child,
-            },
-        ];
+        let parsmsAry;
+        if( child ==undefined){
+           parsmsAry= [
+                {
+                    name: 'isActive',
+                    value: 1,
+                }
+            ]          
+        }else{
+            parsmsAry= [
+                {
+                    name: 'isActive',
+                    value: 1,
+                },{
+                    name: 'categoryChildId',
+                    op: 'like',
+                    value: child,
+                },
+            ]
+        }
+        // if(child){}
+        const WhereConditions = parsmsAry;
+        // console.log(WhereConditions);
         const bannerList: any = await this.bannerService.list(limit, offset, select, search, WhereConditions, count);
         const successResponse: any = {
             status: 1,

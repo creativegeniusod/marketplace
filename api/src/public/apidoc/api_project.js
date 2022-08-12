f9e879890105c16e9e5d1d02962dd1f1ffe27cb4
Ganesh <ganesh@odzservices.com>
2022-08-12 11:56:18 +0530

    change copywrite

--- api/src/public/apidoc/api_project.js
+++ api/src/public/apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2022-08-11T13:01:46.973Z",
+    "time": "2022-08-12T06:03:08.698Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

f9c8bae75e3a6c56e9563bc778ffc4a3f1de2576
mws9987 <mws9987@gmail.com>
2022-08-16 18:26:11 +0530

    home page redesign

--- store/src/default/pages/home/trending-product/trending-product.component.ts
+++ store/src/default/pages/home/trending-product/trending-product.component.ts
@@ -31,7 +31,7 @@ export class TrendingProductComponent implements OnInit, OnDestroy {
   public sidenavOpen = true;
   // card view
   public viewType = 'grid';
-  public viewCol = 25;
+  public viewCol = 14.28;
   public sortings: Array<any>;
   public sortData: any = 'Price Low To High';
   condition: any;
@@ -194,7 +194,7 @@ export class TrendingProductComponent implements OnInit, OnDestroy {
     window.innerWidth < 960
       ? (this.sidenavOpen = false)
       : (this.sidenavOpen = true);
-    window.innerWidth < 1280 ? (this.viewCol = 33.3) : (this.viewCol = 25);
+    window.innerWidth < 1280 ? (this.viewCol = 33.3) : (this.viewCol = 14.28);
   }
 
   // changing the view type

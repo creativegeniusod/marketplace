45bbb14ff3edba213728b792c74e610de9619446
jasvirs210@gmail.com <devtest6785@gmail.com>
2022-08-25 14:50:15 +0530

    new pages ad

--- api/src/public/apidoc/api_project.js
+++ api/src/public/apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2022-08-25T06:26:05.017Z",
+    "time": "2022-08-25T09:19:11.667Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }

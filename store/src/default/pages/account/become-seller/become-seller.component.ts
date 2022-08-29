525eaa3d1fc9fa5accbbf3d321cc51d3462dae1c
Ganesh <ganesh@odzservices.com>
2022-08-29 15:04:13 +0530

    create product page html like home page

---
+++ store/src/default/pages/account/become-seller/become-seller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-become-seller',
+  templateUrl: './become-seller.component.html',
+  styleUrls: ['./become-seller.component.scss']
+})
+export class BecomeSellerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
